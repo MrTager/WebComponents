0b53406321a306364e77d5cdfd728e232f8ecb22
MrTager <mrtager@qq.com>
2024-03-13 10:42:59 +0800

    dev

--- FileSplitUpload/file-upload-vue/public/worker.js
+++ FileSplitUpload/file-upload-vue/public/worker.js
@@ -1,48 +1,49 @@
 import SparkMD5 from 'spark-md5';
 
-onmessage = (e) => {
+onmessage =async (e) => {
   // 获取传递的数据
   const data = e.data;
-  const {chunkSize,file,startIndex,endIndex} = data;
+  const {chunkSize,file,chunkArrStartIndex,chunkArrEndIndex} = data;
   //对文件进行分片
-
-  // let chunkArray = [];
-  // const currentChunk = file.slice(startIndex*chunkSize,endIndex ? endIndex*chunkSize : chunkSize);
-  // const currentChunkCount = endIndex ?  endIndex - startIndex : 1;
-
-  // for(let i = 0; i < currentChunkCount; i++){
-  //   chunkArray.push(currentChunk.slice(i*chunkSize,endIndex ? endIndex*chunkSize : chunkSize));
-  // }
-
-  let chunkArray = [];
-  if(endIndex !== 0){
-    chunkArray =  splitArrayByIndexAndCount(file,startIndex*chunkSize,(endIndex - startIndex)*chunkSize);
-  }else{
-    chunkArray =  file.slice(startIndex*chunkSize);
+  let result = [];
+  for(let i = chunkArrStartIndex; i <= chunkArrEndIndex; i++){
+    // console.log("我是线程开始位置",i)
+      const sChunk = splitArrayByIndexAndCount(file,i,chunkSize);
+      result.push(sChunk);
   }
   
 
-  console.log("文件总大小",file.size)
-  console.log('分片大小',chunkArray);
-  console.log("文件",data)
+  // console.log("文件总大小",file.size)
+  // console.log('分片大小',chunkSize);
+  // console.log("开始位置",chunkArrStartIndex);
+  // console.log("结束位置",chunkArrEndIndex);
 
-  new Promise((resolve,reject) => {
-    resolve(data);
-  }).then((data)=>{
-    postMessage(data);
-  })
+
+  // new Promise((resolve,reject) => {
+  //   resolve(data);
+  // }).then((data)=>{
+    
+  // })
+  postMessage(await Promise.all(result) );
 };
 
-function splitArrayByIndexAndCount(file, startIndex, count) {
-  // 验证输入参数的有效性
-  if (startIndex < 0 || count <= 0 || startIndex >= file.size) {
-    return [];
-  }
-
-  // 计算结束索引，保证不会超过数组长度
-  const endIndex = Math.min(startIndex + count, file.size-1);
-
-  // 使用slice方法返回子数组
-  console.log("第",startIndex,"到",endIndex)
-  return file.slice(startIndex, endIndex);
+function splitArrayByIndexAndCount(file, startIndex, chunckSize) {
+  return new Promise((resolve,reject) => {
+    const start = startIndex*chunckSize
+    const end = Math.min( start + chunckSize,file.size);
+    const spark = new SparkMD5.ArrayBuffer();
+    const fileReader = new FileReader();
+    const blob = file.slice(start,end);
+    fileReader.onload = (e) => {
+      spark.append(e.target.result);
+      resolve({
+        hash: spark.end(),
+        index: startIndex,
+        start,
+        end,
+        blob
+      })
+    }
+    fileReader.readAsArrayBuffer(blob);
+  })
 }
